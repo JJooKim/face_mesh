68158efa0a576bf2262f1ee58771536a57f9a78e
JJooKim <rladudwn0625@gmail.com>
2023-11-27 22:22:34 +0900

    draw_utils changed for mesh

--- draw_utils.py
+++ draw_utils.py
@@ -6,30 +6,30 @@ from mesh_connection import FACE_CONNECTIONS
 
 def draw_face(image, bboxes, landmarks, scores, confidence=False):
     image_ret = image.copy()
-    for bbox, landmark, score in zip(bboxes.astype(int), landmarks.astype(int), scores):
-        image_ret = cv2.rectangle(image_ret, tuple(bbox[:2]), tuple(bbox[2:]), color=(255, 0, 0), thickness=2)
-        landmark = landmark.reshape(-1, 2)
-        for i, (p, color) in enumerate(zip(landmark,
-                                           [[255, 0, 0],
-                                            [0, 255, 0],
-                                            [0, 0, 255],
-                                            [255, 255, 0],
-                                            [255, 0, 255],
-                                            [0, 255, 255]])):
-            image_ret = cv2.circle(image_ret, tuple(p), 3, color, thickness=2)
-            image_ret = cv2.putText(image_ret, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, color=(255, 255, 255), fontScale=1)
-        if confidence:
-            score_label = "{:.2f}".format(score)
-            (label_width, label_height), baseline = cv2.getTextSize(score_label,
-                                                                    cv2.FONT_HERSHEY_SIMPLEX,
-                                                                    fontScale=1.0,
-                                                                    thickness=2)
-            label_btmleft = bbox[:2].copy() + 10
-            label_btmleft[0] += label_width
-            label_btmleft[1] += label_height
-            cv2.rectangle(image_ret, tuple(bbox[:2]), tuple(label_btmleft), color=(255, 0, 0), thickness=cv2.FILLED)
-            cv2.putText(image_ret, score_label, (bbox[0] + 5, label_btmleft[1] - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, color=(255, 255, 255), thickness=2)
+    # for bbox, landmark, score in zip(bboxes.astype(int), landmarks.astype(int), scores):
+    #     image_ret = cv2.rectangle(image_ret, tuple(bbox[:2]), tuple(bbox[2:]), color=(255, 0, 0), thickness=2)
+    #     landmark = landmark.reshape(-1, 2)
+    #     for i, (p, color) in enumerate(zip(landmark,
+    #                                        [[255, 0, 0],
+    #                                         [0, 255, 0],
+    #                                         [0, 0, 255],
+    #                                         [255, 255, 0],
+    #                                         [255, 0, 255],
+    #                                         [0, 255, 255]])):
+    #         image_ret = cv2.circle(image_ret, tuple(p), 3, color, thickness=2)
+    #         image_ret = cv2.putText(image_ret, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, color=(255, 255, 255), fontScale=1)
+    #     if confidence:
+    #         score_label = "{:.2f}".format(score)
+    #         (label_width, label_height), baseline = cv2.getTextSize(score_label,
+    #                                                                 cv2.FONT_HERSHEY_SIMPLEX,
+    #                                                                 fontScale=1.0,
+    #                                                                 thickness=2)
+    #         label_btmleft = bbox[:2].copy() + 10
+    #         label_btmleft[0] += label_width
+    #         label_btmleft[1] += label_height
+    #         cv2.rectangle(image_ret, tuple(bbox[:2]), tuple(label_btmleft), color=(255, 0, 0), thickness=cv2.FILLED)
+    #         cv2.putText(image_ret, score_label, (bbox[0] + 5, label_btmleft[1] - 5),
+    #                     cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, color=(255, 255, 255), thickness=2)
     return image_ret
 
 
